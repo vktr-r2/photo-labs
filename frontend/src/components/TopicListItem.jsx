a8b95460f34c59567f01cd927a7e111e54afb72e
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-18 16:58:56 -0400

    Design TopicListItem link component

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
-import '../styles/TopicListItem'
+import '../styles/TopicListItem.scss'
 
-const TopicListItem = () => {
+const TopicListItem = (props) => {
+  const {id, label, link} = props;
+
+  return (
   <div className="topic-list--item">
-    {/* Insert React */}
+    <a href={link} key={id} className="topic-list--item-link"> 
+      <span>{label}</span> 
+    </a>
   </div>
+  )
 }
 
-TopicListItem.defaultProps = {
-  id: 1,
-  label: 'Nature',
-  link: 'link placeholder' 
-}
 export default TopicListItem
+
+// TopicListItem.defaultProps = {
+//   id: 1,
+//   label: 'Nature',
+//   link: 'link placeholder' 
+// }
