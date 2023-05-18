48d9dab1260960549102c86e638a9b076c20efc8
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-18 17:18:30 -0400

    Design TopNavigationBar component to include logo, TopicList, and FavBadge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
+import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar--logo">PhotoLabs</span>
+      <TopicList/>
+      <FavBadge/>
     </div>
   )
 }
