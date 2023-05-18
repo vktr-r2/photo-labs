3dc1eed5c18f3719c129122db97e6c0922aa8c54
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-18 16:59:37 -0400

    Design TopicList to display multiple TopicListItem components using .map

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import './TopicList.scss';
+import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
-const TopicList = () => {
-  <div className="top-nav-bar--topic-list">
-    {/* Insert React */}
-  </div>
-}
+const TopicList = (props) => {
+  const TopicListComponentsArr = props.topics.map((topic) => {
+    return (
+      <TopicListItem
+        link={topic.link}
+        label={topic.label}
+        key={topic.id}
+        className="topic-list--item"
+      />
+    );
+  });
+  console.log(TopicListComponentsArr);
+  return <div className="top-nav-bar--topic-list" >{TopicListComponentsArr}</div>;
+};
 
 TopicList.defaultProps = {
   topics: [
     {
       id: 1,
-      label: 'Nature',
-      link: 'link placeholder' 
+      label: "Nature",
+      link: "link placeholder",
     },
-    { 
-      id: 2, 
-      label: 'Food',
-      link: 'link placeholder' 
+    {
+      id: 2,
+      label: "Food",
+      link: "link placeholder",
     },
     {
       id: 3,
-      label: 'People',
-      link: 'link placeholder' 
+      label: "People",
+      link: "link placeholder",
     },
-  ]
-}
-export default TopicList
+  ],
+};
+export default TopicList;
