72893f5e4b612da423ce697ac0b93f2f5178524c
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-19 13:27:59 -0400

    Add favPhotos default state, define addFavPhoto func to update state

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import '../styles/HomeRoute.scss';
 import TopNavigation from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 
-const HomeRoute = (props) => {
-  // console.log(props)
 
+//HomeRoute takes props topics and photos data from App.jsx
+const HomeRoute = (props) => {
+  //Destructure data into variables
   const {topics, photos} = props;
 
+  // set useState to empty obj | assign empty obj state to favPhotos | declare setFavPhotos function to update state
+  const [favPhotos, setFavPhotos] = useState({});
+
+  // addFavPhoto takes a photoId as its arg
+  const addFavPhoto = (photoId) => {
+    //prevFavPhotos equals the current state of favPhotos object (default functionality from setFavPhotos)
+    setFavPhotos(prevFavPhotos => {
+      //updatedFavPhotos equals a copy of prevFavPhotos(current state) and adds [photoId] (from the liked photo):true property to signal that the photo is favourited
+      const updatedFavPhotos = { ...prevFavPhotos, [photoId]: true };
+      // console.log(updatedFavPhotos); // TEST Print updated state of favPhotos after favouriting a photo (WORKS)
+      //return updatedFavPhotos as the new state for favPhotos
+      return updatedFavPhotos;
+    });
+  };
+
+
   return (
-  <div className="home-route">
-    <TopNavigation topics={topics}/>
-    <PhotoList photos={photos}/>
-  </div>
+    <div className="home-route">
+      <TopNavigation topics={topics}/>
+      <PhotoList 
+        photos={photos} 
+        favPhotos={favPhotos} 
+        addFavPhoto={addFavPhoto} 
+      />
+    </div>
   );
 }
 
