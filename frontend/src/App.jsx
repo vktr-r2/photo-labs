c03c89b608a1e15d1da0e007f79f1ab24146171a
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-18 20:38:58 -0400

    Build HomeRoute component to house NavBar and PhotoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
+import PhotoFavButton from './components/PhotoFavButton';
+import TopicListItem from './components/TopicListItem';
+import TopicList from './components/TopicList';
+import TopNavigation from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
+
+
+
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
-const App = () => (
+const App = () => {
+  return (
   <div className="App">
     {/* <PhotoListItem/> */}
-    <PhotoList/>
+    {/* <PhotoFavButton/> */}
+    {/* <TopicListItem/> */}
+    {/* <TopicList/> */}
+    {/* <TopNavigation/> */}
+    {/* <PhotoList/> */}
+    <HomeRoute/>
+
   </div>
-)
+)}
 
 export default App
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigation from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
 
 const HomeRoute = () => 
   <div className="home-route">
-    {/* Insert React */}
+    <TopNavigation/>
+    <PhotoList/>
   </div>
 
 export default HomeRoute;
