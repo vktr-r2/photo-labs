a8b3e7f1dcfe67d2e79a0c1d0a9fea75a5a4a3d2
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-19 17:46:40 -0400

    Read favPhotos state prop and adjust FavIcon styling accordingly

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,19 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-export const FavBadge = ({ isFavPhotoExist }) => {
+export const FavBadge = (props) => {
+  
+  //favPhotos state drilled down from HomeRoute > object shows which photos (if any) have been liked
+  const { favPhotos } = props;
+
+  //isFavPhotoExist function designed to check if favPhotos object is empty or not.  Returns TRUE if ANY photo has been liked
+  const isFavPhotoExist = (object) => {
+    return Object.keys(object).length > 0;
+  }
+
   return (
     <div className='fav-badge'>
-      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist}/>
+      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist(favPhotos)}/>
     </div>
   ) 
 };
