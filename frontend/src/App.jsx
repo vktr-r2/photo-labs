918ec5bee864fc35ff078e2175f5059b778eadd3
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-20 10:16:44 -0400

    Define showModal and clickedPhoto states to control modal rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
@@ -7,14 +7,28 @@ import TopicListItem from './components/TopicListItem';
 import TopicList from './components/TopicList';
 import TopNavigation from './components/TopNavigationBar';
 import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 
 import topics from './mocks/topics.json';
 import photos from './mocks/photos.json';
 
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  //State controls whether modal should be active or not
+  const [showModal, setShowModal] = useState(false);
+  //State controls the photo data that will be passed to the modal
+  const [clickedPhoto, setClickedPhoto] = useState(null);
+
+  //Function to be drilled down to PhotoListItem component, captures photoProps of photo that is clicked and sets showModal to TRUE
+  const handlePhotoClick = (photoProps) => {
+    setClickedPhoto(photoProps);
+    setShowModal(true);
+    // console.log(photoProps) //TEST to see if handlePhotoClick is being passed photoProps arg successfully - WORKS
+  };
+
+
 
   return (
   <div className="App">
@@ -24,7 +38,9 @@ const App = () => {
     {/* <TopicList/> */}
     {/* <TopNavigation/> */}
     {/* <PhotoList/> */}
-    <HomeRoute topics={topics} photos={photos}/>
+    <HomeRoute topics={topics} photos={photos} onPhotoClick={handlePhotoClick}/>
+    {/*Conditional checks if showModal true, if true render modal*/ }
+    {showModal && <PhotoDetailsModal photo={clickedPhoto} />}
 
   </div>
 )}
