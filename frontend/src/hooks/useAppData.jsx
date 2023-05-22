fae42fdb7ca5d36f303a011d3948053cc04a45e6
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-22 17:32:31 -0400

    Setup ACTIONS object and define reducer function

--- frontend/src/hooks/useAppData.jsx
+++ frontend/src/hooks/useAppData.jsx
@@ -1,75 +1,50 @@
-import { useState } from 'react';
+import React, { useReducer } from "react";
+import topics from "../mocks/topics.json";
+import photos from "../mocks/photos.json";
 
-//Import mock data from JSON files
-import topics from '../mocks/topics.json';
-import photos from '../mocks/photos.json';
-
-const useApplicationData = () => {
-  // showModal boolean state, handles if PhotoDetailsModal should render or not
-  const [showModal, setShowModal] = useState(false);
-
-  // clickedPhoto stores photo data for which photo is clicked, intended to be passed to PDModal
-  const [clickedPhoto, setClickedPhoto] = useState(null);
-
-  // favPhotos object intended to store which photos have been favourited.  Object properties formatted as [photo.id=TRUE]
-  const [favPhotos, setFavPhotos] = useState({});
-
-  // set mock data states to be passed down through components
-  const [topicsData, setTopicsData] = useState(topics);
-  const [photosData, setPhotosData] = useState(photos);
-
-  // handePhotoClick uses photoProps of the clicked photo to capture photo details that need to be passed to PDModal
-  const handlePhotoClick = (photoProps) => {
-    // Change clickedPhoto state to the props captured by click
-    setClickedPhoto(photoProps);
-    // Change showModal boolean to TRUE to render component
-    setShowModal(true);
-  };
-
-  // closeModal triggered on 'x' button click in the modal
-  const closeModal = () => {  
-    // showModal state set to FALSE so modal no longer rendered
-    setShowModal(false);
-    // empty the clickedPhoto state so no issues caused on next click of any photo
-    setClickedPhoto(null);
-  };
-
-  // addFavPhoto takes a photoId as its arg and adds the photoId to favPhotos state
-  const addFavPhoto = (photoId) => {
-    // prevFavPhotos equals the current state of favPhotos object (default functionality from setFavPhotos)
-    setFavPhotos((prevFavPhotos) => {
-      // updatedFavPhotos equals a copy of prevFavPhotos(current state) and adds [photoId] (from the liked photo):true property to signal that the photo is favourited
-      const updatedFavPhotos = { ...prevFavPhotos, [photoId]: true };
-      // return updatedFavPhotos as the new state for favPhotos
-      return updatedFavPhotos;
-    });
-  };
-
-  // removeFavPhoto takes a photoId as arg and removes the photoId key:value property from favPhoto state obj
-  const removeFavPhoto = (photoId) => {
-    // prevFavPhotos equals the current state of favPhotos object
-    setFavPhotos((prevFavPhotos) => {
-      // Make a copy of current favPhotos object using spread operator
-      const updatedFavPhotos = { ...prevFavPhotos };
-      // Remove the key:value property associated with the photoId arguement
-      delete updatedFavPhotos[photoId];
-      // return updatedFavPhotos as the new state for favPhotos
-      return updatedFavPhotos;
-    });
-  };
-
-  return {
-    showModal,
-    clickedPhoto,
-    favPhotos,
-    handlePhotoClick,
-    closeModal,
-    addFavPhoto,
-    removeFavPhoto,
-    topicsData,
-    photosData,
-  };
+//ACTIONS is object full of strings that help us pass instruction to dispatch function later on
+//Important to set these as strings in an object >> in case of typo JS will advise we're using an undefined constant, where as spelling out the string manually each time could make it hard to find a typo
+export const ACTIONS = {
+  SHOW_MODAL: "SHOW_MODAL",
+  CLOSE_MODAL: "CLOSE_MODAL",
+  SET_CLICKED_PHOTO: "SET_CLICKED_PHOTO",
+  ADD_FAV_PHOTO: "ADD_FAV_PHOTO",
+  REMOVE_FAV_PHOTO: "REMOVE_FAV_PHOTO",
 };
 
-export default useApplicationData;
+//Reducer takes the current state object as an arguement (object stores all different states for our app) and an action object as well, which is expected to have a "type" property.  "action" parameter describes the change we need to apply to the state
+function reducer(state, action) {
 
+  //Switch/case statement acts as a giant IF/ELSE statement.  It checks the value of action.type property, and will return what changes need to be applied to the state object based on the value given.
+  switch (action.type) {
+    
+    //In the case of ACTIONS.SHOW_MODAL: return a new state object that is a copy of the old state object, but showModal property is set to TRUE
+    case ACTIONS.SHOW_MODAL:
+      return { ...state, showModal: true };
+
+    //Case of ACTIONS.CLOSE_MODAL: return copy of state object, set showModal: false and clickedPhoto: NULL to empty stored photo data (so modal can be reopened on another photo)
+    case ACTIONS.CLOSE_MODAL:
+      return { ...state, showModal: false, clickedPhoto: NULL };
+
+    //Case of ACTIONS.SET_CLICKED_PHOTO: return copy of state object, and set clickedPhoto property to equal value of action.payload.
+    //actions (the arguement passed to reducer) are often defined as plain objects with a TYPE property and a PAYLOAD property. The type describes the kind of state change, and the payload field contains any data needed for the update.
+    //In the case of ACTIONS.SET_CLICKED_PHOTO, action.payload is expected to contain the data of the clicked photo. When this action is dispatched, the clickedPhoto state will be updated with the data provided in action.payload
+    case ACTIONS.SET_CLICKED_PHOTO:
+      return { ...state, clickedPhoto: action.payload };
+
+    //Case of ACTIONS.ADD_FAV_PHOTO: return copy of state.  Since favPhotos is an object within state, return copy of favPhotos object, and add [action.payload] (action.payload is us using the photo ID as the key): true (true meaning the photo has been favourited)
+    case ACTIONS.ADD_FAV_PHOTO:
+      return {...state, favPhotos: {...state.favPhotos, [action.payload]: true}}
+    
+    case ACTIONS.REMOVE_FAV_PHOTO:
+      //Make a copy of favPhotos object
+      let updatedFavPhotos = {...state.favPhotos};
+      //In copy of object, delete the key/value pair where the key matches the photoID for the unliked photo
+      delete updatedFavPhotos[action.payload];
+      //Return a copy of the current state object, and set favPhotos object within it to equal new updatedFavPhotos
+      return {...state, favPhotos: updatedFavPhotos}
+    
+    default:
+      throw new Error (`Unable to reduce with unsupported action type: ${action.type}`);
+  }
+}
