702f8b050926edcc0200be7961a8e81c23b4c105
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-23 15:39:37 -0400

    Refactor useEffect to call photo data based on value of state.topic

--- frontend/src/hooks/useAppData.jsx
+++ frontend/src/hooks/useAppData.jsx
@@ -15,10 +15,8 @@ export const ACTIONS = {
 
 //Reducer takes the current state object as an arguement (object stores all different states for our app) and an action object as well, which is expected to have a "type" property.  "action" parameter describes the change we need to apply to the state
 const reducer = (state, action) => {
-
   //Switch/case statement acts as a giant IF/ELSE statement.  It checks the value of action.type property, and will return what changes need to be applied to the state object based on the value given.
   switch (action.type) {
-    
     //In the case of ACTIONS.SHOW_MODAL: return a new state object that is a copy of the old state object, but showModal property is set to TRUE
     case ACTIONS.SHOW_MODAL:
       return { ...state, showModal: true };
@@ -35,36 +33,40 @@ const reducer = (state, action) => {
 
     //Case of ACTIONS.ADD_FAV_PHOTO: return copy of state.  Since favPhotos is an object within state, return copy of favPhotos object, and add [action.payload] (action.payload is us using the photo ID as the key): true (true meaning the photo has been favourited)
     case ACTIONS.ADD_FAV_PHOTO:
-      return {...state, favPhotos: {...state.favPhotos, [action.payload]: true}}
-    
+      return {
+        ...state,
+        favPhotos: { ...state.favPhotos, [action.payload]: true },
+      };
+
     case ACTIONS.REMOVE_FAV_PHOTO:
       //Make a copy of favPhotos object
-      let updatedFavPhotos = {...state.favPhotos};
+      let updatedFavPhotos = { ...state.favPhotos };
       //In copy of object, delete the key/value pair where the key matches the photoID for the unliked photo
       delete updatedFavPhotos[action.payload];
       //Return a copy of the current state object, and set favPhotos object within it to equal new updatedFavPhotos
-      return {...state, favPhotos: updatedFavPhotos}
+      return { ...state, favPhotos: updatedFavPhotos };
 
-      case ACTIONS.SET_TOPICS:
-        // In the case of ACTIONS.SET_TOPICS, return a new state object that is a copy of the old state object, but topicsData property is set to action.payload
-        return {...state, topicsData: action.payload};
-  
-      case ACTIONS.SET_PHOTOS:
-        // In the case of ACTIONS.SET_PHOTOS, return a new state object that is a copy of the old state object, but photosData property is set to action.payload
-        return {...state, photosData: action.payload};
+    case ACTIONS.SET_TOPICS:
+      // In the case of ACTIONS.SET_TOPICS, return a new state object that is a copy of the old state object, but topicsData property is set to action.payload
+      return { ...state, topicsData: action.payload };
 
-      case ACTIONS.SET_TOPIC:
-          //action.payload is the topicId of the clicked on topic in TopicListItem
-          return { ...state, topic: action.payload };
-    
-      //Catches all action.types that have not had cases defined for them above
+    case ACTIONS.SET_PHOTOS:
+      // In the case of ACTIONS.SET_PHOTOS, return a new state object that is a copy of the old state object, but photosData property is set to action.payload
+      return { ...state, photosData: action.payload };
+
+    case ACTIONS.SET_TOPIC:
+      //action.payload is the topicId of the clicked on topic in TopicListItem
+      return { ...state, topic: action.payload };
+
+    //Catches all action.types that have not had cases defined for them above
     default:
-      throw new Error (`Unable to reduce with unsupported action type: ${action.type}`);
+      throw new Error(
+        `Unable to reduce with unsupported action type: ${action.type}`
+      );
   }
-}
+};
 
 export const useApplicationData = () => {
-  
   //initialState object stores the default state for all states
   const initialState = {
     showModal: false,
@@ -72,93 +74,86 @@ export const useApplicationData = () => {
     favPhotos: {},
     topicsData: [],
     photosData: [],
-    topic: undefined
+    topic: undefined,
   };
 
   //Destructuring state, dispatch values from the array that useReducer returns
-  const [state, dispatch] = useReducer(reducer,initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-// useEffect makes two separate fetch calls after page loads (Happens once)
-useEffect(() => {
-  fetch('/api/topics')
-    // parse JSON response received
-    .then(response => response.json())
-    // data = the parsed JSON response
-    .then(data => {
-      // Use dispatch to update the state properly
-      dispatch({ type: ACTIONS.SET_TOPICS, payload: data });
-    })
-    // Catch any errors and log them
-    .catch(error => {
-      console.error('Error fetching topics:', error);
-    });
+  // useEffect makes two separate fetch calls and updates each time state.topic changes
+  useEffect(() => {
 
-  fetch('/api/photos')
-    .then(response => response.json())
-    .then(data => {
-      // Use dispatch to update the state properly
-      dispatch({ type: ACTIONS.SET_PHOTOS, payload: data });
-    })
-    // Catch any errors and log them
-    .catch(error => {
-      console.error('Error fetching photos:', error);
-    });
-}, []);
+    //Set URL to lead to main photo content
+    let url = "/api/photos";
 
-//useEffect calls results API each time state.topic value changes
-useEffect(() => {
-  //if state.topic is truthy, make fetch call
-  if (state.topic) {
-    fetch(`http://localhost:8001/api/topics/photos/${state.topic}`)
-      .then(response => response.json())
-      .then(data => {
-        //dispatch will set SET_PHOTOS state to equal just the photos tied to the topic id
+    //If state topic is not undefined, then set URL to lead to topic photo content
+    if (state.topic !== undefined) {
+      url = `http://localhost:8001/api/topics/photos/${state.topic}`;
+    }
+
+    fetch("/api/topics")
+      // parse JSON response received
+      .then((response) => response.json())
+      // data = the parsed JSON response
+      .then((data) => {
+        // Use dispatch to update the state properly
+        dispatch({ type: ACTIONS.SET_TOPICS, payload: data });
+      })
+      // Catch any errors and log them
+      .catch((error) => {
+        console.error("Error fetching topics:", error);
+      });
+    
+    //fetch url variable (depends on whether topic is set or not)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        //dispatch the photo data that is fetched
         dispatch({ type: ACTIONS.SET_PHOTOS, payload: data });
       })
-      .catch(error => {
-        console.error(`Error fetching photos for topic ${state.topic}:`, error);
+      .catch((error) => {
+        console.error("Error fetching photos:", error);
       });
-  }
-}, [state.topic]);
-
+      //if state.topic dependancy changes, call useEffect again
+  }, [state.topic]);
 
   //handlePhotoClick accepts photoProps as argument
   const handlePhotoClick = (photoProps) => {
     // dispatch call triggers an action in the reducer function
     // SET_CLICKED_PHOTO sets value of clickedPhoto state in initialState obj to equal photoProps from clicked photo
     // photoProps payload captured by onClick listener set on the <img> element in PhotoListItem component
-    dispatch({ type: ACTIONS.SET_CLICKED_PHOTO, payload: photoProps});
+    dispatch({ type: ACTIONS.SET_CLICKED_PHOTO, payload: photoProps });
     //SHOW_MODAL sets showModal value to TRUE in PhotoListItem
-    dispatch({ type: ACTIONS.SHOW_MODAL});
+    dispatch({ type: ACTIONS.SHOW_MODAL });
   };
 
   const closeModal = () => {
     //CLOSE_MODAL sets showModal to FALSE< and clickedPhoto to NULL
-    dispatch({type: ACTIONS.CLOSE_MODAL});
+    dispatch({ type: ACTIONS.CLOSE_MODAL });
   };
 
   //accepts photoId from props passed up from photo where the favourite icon is clicked
   const addFavPhoto = (photoId) => {
     //ADD_FAV_PHOTO adds a property to the favPhotos obj formatted as "[photoId]: true"
-    dispatch({type: ACTIONS.ADD_FAV_PHOTO, payload: photoId});
+    dispatch({ type: ACTIONS.ADD_FAV_PHOTO, payload: photoId });
   };
 
   //accepts photoId from props passed up from photo where the favourite icon is clicked
   const removeFavPhoto = (photoId) => {
     //REMOVE_FAV_PHOTO deletes property from favPhotos obj where key matches the photoID arguement received
-    dispatch({type: ACTIONS.REMOVE_FAV_PHOTO, payload: photoId});
+    dispatch({ type: ACTIONS.REMOVE_FAV_PHOTO, payload: photoId });
   };
 
   //accepts topicId from props passed up from TopicListItem is clicked
   const updateTopic = (topicId) => {
-    //SET_TOPIC deletes 
+    //SET_TOPIC deletes
     dispatch({ type: ACTIONS.SET_TOPIC, payload: topicId });
   };
 
   //rese topic state back to undefined to view unfiltered list of photos
   const resetTopic = () => {
     dispatch({ type: ACTIONS.SET_TOPIC, payload: undefined });
-};
+  };
 
   //Return current state along with dispatch functions
   return {
@@ -167,8 +162,7 @@ useEffect(() => {
     closeModal,
     addFavPhoto,
     removeFavPhoto,
-    updateTopic
+    updateTopic,
+    resetTopic,
   };
-
 };
-
