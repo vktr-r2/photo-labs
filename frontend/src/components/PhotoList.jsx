9ecfdd081f130466b26507708a81d43d3299708a
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-28 20:07:26 -0400

    Add showFavOnly filter to Array component

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -11,24 +11,26 @@ const PhotoList = (props) => {
   //Declare new PhotoListComponentsArr, check if "photos" is array, if true then use .map to create each PhotoListItem component
   const PhotoListComponentsArr =
     Array.isArray(photos) &&
-    photos.map((photo) => {
-      return (
-        <PhotoListItem
-          id={photo.id}
-          key={photo.id}
-          username={photo.user.username}
-          imageSource={photo.urls.regular}
-          description={photo.description}
-          avatar={photo.user.profile}
-          city={photo.location.city}
-          country={photo.location.country}
-          isFav={favPhotos[photo.id]} // Boolean indicating whether photo is favourited based on whether photo.id exists in favPhotos obj
-          onFavClick={() => addFavPhoto(photo.id)} // Pass function with photo.id as arg to addFavPhoto
-          onUnFavClick={() => removeFavPhoto(photo.id)} // Pass function with photo.id as arg to removeFavPhoto
-          onPhotoClick={onPhotoClick}
-        />
-      );
-    });
+    photos
+      .filter((photo) => !props.showFavOnly || props.favPhotos[photo.id])
+      .map((photo) => {
+        return (
+          <PhotoListItem
+            id={photo.id}
+            key={photo.id}
+            username={photo.user.username}
+            imageSource={photo.urls.regular}
+            description={photo.description}
+            avatar={photo.user.profile}
+            city={photo.location.city}
+            country={photo.location.country}
+            isFav={favPhotos[photo.id]} // Boolean indicating whether photo is favourited based on whether photo.id exists in favPhotos obj
+            onFavClick={() => addFavPhoto(photo.id)} // Pass function with photo.id as arg to addFavPhoto
+            onUnFavClick={() => removeFavPhoto(photo.id)} // Pass function with photo.id as arg to removeFavPhoto
+            onPhotoClick={onPhotoClick}
+          />
+        );
+      });
   //props.children rendered here so PhotoList can render photos in PhotoDetailsModal.jsx
   return (
     <ul className="photo-list">{props.children || PhotoListComponentsArr}</ul>
