1d2dd39ec70cc3d123059966fad06e2e81f99192
Viktor Ristic <vktr.r2@protonmail.com>
2023-10-04 22:01:08 -0400

    Pass showFavOnly, toggleShowFavOnly to PhotoList and TopNavigation

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -14,6 +14,8 @@ const HomeRoute = (props) => {
     removeFavPhoto, //Function that removes photo from favPhotos Obj
     updateTopic, //Function captures topic ID for API call in useAppData.jsx
     resetTopic, //Function resets the state of topic back to undefined to remove filter
+    showFavOnly, //State manages favourites filter
+    toggleShowFavOnly, //Function toggles showFavOnly state on/off
   } = props;
 
   return (
@@ -23,6 +25,7 @@ const HomeRoute = (props) => {
         favPhotos={favPhotos}
         updateTopic={updateTopic}
         resetTopic={resetTopic}
+        toggleShowFavOnly={toggleShowFavOnly}
       />
       <PhotoList
         photos={photos}
@@ -30,6 +33,7 @@ const HomeRoute = (props) => {
         addFavPhoto={addFavPhoto}
         removeFavPhoto={removeFavPhoto}
         onPhotoClick={onPhotoClick}
+        showFavOnly={showFavOnly}
       />
     </div>
   );
